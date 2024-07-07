10ae51356b0f9b211331337146c2103bf662f8e9
ScottSauceda <marcus.sauceda@codeimmersives.com>
2024-07-07 16:01:12 -0500

    first commit

---
+++ byte-sized/src/main/java/com/byte_sized/byte_sized/ByteSizedApplication.java
@@ -0,0 +1,13 @@
+package com.byte_sized.byte_sized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteSizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteSizedApplication.class, args);
+	}
+
+}
---
+++ byte-sized/src/test/java/com/byte_sized/byte_sized/ByteSizedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byte_sized.byte_sized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteSizedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
